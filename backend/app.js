2580e7c79403bae9a7f87d91334d0264a13ca059
shahal <shahal.c077@gmail.com>
2026-03-07 02:36:23 +0530

    update production API

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,8 @@ const app = express();
 // CORS FIX
 app.use(cors({
   origin: [
-    "http://localhost:5173",   // Vite local frontend
-    "http://localhost:3000",   // React dev server (optional)
-    "https://olx-clone-ochre.vercel.app" // your deployed frontend
+    "http://localhost:5173",
+    "https://olx-clone-ochre.vercel.app"
   ]
 }));
 
@@ -19,9 +18,6 @@ app.use(cors({
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-app.get("/", (req, res) => {
-  res.send("OLX API Running 🚀");
-});
 
 // Routes
 app.use("/api/auth", authRoutes);
--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "https://olx-clone-fnky.onrender.com/api",
+ baseURL: "https://olx-clone-fmky.onrender.com/api",
   headers: {
     "Content-Type": "application/json"
   }
