ef997ea0e7eb3dd0ec492a6744ac94635dc921fa
shahal <shahal.c077@gmail.com>
2026-03-06 15:18:41 +0530

    Refactor code structure for improved readability and maintainability

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,14 @@ const productRoutes = require("./routes/productRoutes");
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",   // Vite local frontend
+    "http://localhost:3000",   // React dev server (optional)
+    "https://olx-clone-ochre.vercel.app" // your deployed frontend
+  ]
+}));
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
